786b85c9c89f93ccde1763b62c67bf6ecf4609f0
Cleiton Cunha <cleiton_sc_04@hotmail.com>
2026-09-21 21:20:59 -0300

    test: retorna o próprio número quando há um só

--- packages/sandbox/src/string-calculator/StringCalculator.test.ts
+++ packages/sandbox/src/string-calculator/StringCalculator.test.ts
@@ -5,4 +5,8 @@ describe("StringCalculator", () => {
 	it("should return 0 for an empty string", () => {
 		expect(StringCalculator.add("")).toBe(0);
 	});
+
+	it("should return the number for a single number string", () => {
+		expect(StringCalculator.add("5")).toBe(5);
+	});
 });
--- packages/sandbox/src/string-calculator/StringCalculator.ts
+++ packages/sandbox/src/string-calculator/StringCalculator.ts
@@ -1,5 +1,5 @@
 export class StringCalculator {
 	static add(numbers: string): number {
-		return 0;
+		return Number(numbers);
 	}
 }
